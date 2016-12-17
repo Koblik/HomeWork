9c2c58157dec80aeb7a6d7df565139326545ee64
Roman Kor <romses1978@gmail.com>
2016-12-17 22:09:25 +0200

    i have change the file

--- src/com/koblik/HelloWorld.java
+++ src/com/koblik/HelloWorld.java
@@ -2,6 +2,7 @@ package com.koblik;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hi people!!!");
+		System.out.println("My Name Roman");
 	}
 }
